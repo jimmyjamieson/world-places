eb4f72428bfee0b1c83947119e2779dca3688da0
Ian Jamieson <ian.jamieson@globalgaming.com>
2020-07-04 18:36:31 +0200

    port to 4000 for api

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(4000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
