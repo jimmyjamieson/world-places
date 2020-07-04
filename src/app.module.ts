b201d602d7da90f2b20af3c6fe7fd523f94ad0bf
Ian Jamieson <ian.jamieson@globalgaming.com>
2020-07-04 17:16:46 +0200

    generate required modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContinentsModule } from './continents/continents.module';
+import { CountriesModule } from './countries/countries.module';
+import { RegionsModule } from './regions/regions.module';
+import { CitiesModule } from './cities/cities.module';
+import { CurrenciesModule } from './currencies/currencies.module';
+import { LanguagesModule } from './languages/languages.module';
 
 @Module({
   imports: [
@@ -13,6 +18,11 @@ import { ContinentsModule } from './continents/continents.module';
       synchronize: true,
     }),
     ContinentsModule,
+    CountriesModule,
+    RegionsModule,
+    CitiesModule,
+    CurrenciesModule,
+    LanguagesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
