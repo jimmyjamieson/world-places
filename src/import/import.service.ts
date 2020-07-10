b0e5cafc6bd44d83fd3f681ac9417d5432a15e22
Ian Jamieson <ian.jamieson@globalgaming.com>
2020-07-10 16:14:16 +0200

    data import fixes

--- src/import/import.service.ts
+++ src/import/import.service.ts
@@ -1,32 +1,18 @@
 import { createReadStream } from 'fs';
 import { Injectable } from '@nestjs/common';
-import { CsvParser, ParsedData } from 'nest-csv-parser';
 import csv from 'csv-parser';
 import { Country } from '../countries/country.entity';
-import removeUnicode from '../utils/remove-unicode';
 import { getManager } from 'typeorm';
-import { CountriesService } from '../countries/countries.service';
-import { ContinentsService } from '../continents/continents.service';
 import { Currency } from '../currencies/currencies.entity';
-import { CurrenciesService } from '../currencies/currencies.service';
 import { Continent } from '../continents/continent.entity';
 import { Region } from '../regions/region.entity';
 import { Language } from '../languages/languages.entity';
-import { LanguagesService } from '../languages/languages.service';
 import { City } from '../cities/cities.entity';
-import { RegionsService } from '../regions/regions.service';
 import { continents } from './data';
 
 @Injectable()
 export class ImportService {
-  constructor(
-    private readonly csvParser: CsvParser,
-    private readonly countriesService: CountriesService,
-    private readonly continentsService: ContinentsService,
-    private readonly regionsService: RegionsService,
-    private readonly currenciesService: CurrenciesService,
-    private readonly languagesService: LanguagesService,
-  ) {}
+  constructor() {}
 
   entityManager = getManager();
   async getAllContinents() {
@@ -45,20 +31,8 @@ export class ImportService {
     return await this.entityManager.find(Currency);
   }
 
-  async importFile(entity, file) {
-    const stream = await createReadStream(file);
-    const csv: ParsedData<any> = await this.csvParser.parse(
-      stream,
-      entity,
-      null,
-      null,
-      {
-        strict: true,
-        separator: ',',
-        mapHeaders: ({ header, index }) => header.toLowerCase(),
-      },
-    );
-    return csv;
+  async startImportCsv() {
+    await this.importCurrencies()
   }
 
   async importCurrencies() {
@@ -79,7 +53,8 @@ export class ImportService {
           decimals,
         };
         this.entityManager.save(Currency, obj);
-      });
+      })
+      .on('end', () => this.importLanguages());
   }
 
   async importLanguages() {
@@ -91,18 +66,20 @@ export class ImportService {
         }),
       )
       .on('data', data => {
-        const { code, name, nativeName } = data;
+        const { code, name, nativeName, code2 } = data;
         const obj = {
           name,
           nativeName: nativeName || name,
           code,
+          code_iso_3: code2,
         };
         this.entityManager.save(Language, obj);
-      });
+      }).on('end', () => this.importContinents());
   }
 
   async importContinents() {
     await this.entityManager.save(Continent, [...continents]);
+    await this.importCountries()
   }
 
   async importCountries() {
@@ -138,8 +115,10 @@ export class ImportService {
         } = data;
 
         const continent = continents.find(item => item.name === continentName);
-        const currency = currencies.find(item => item.code === currencyCode)
-        const language = languages.find(item => item.code === languageCode)
+        const currency = currencies.find(item => item.code === currencyCode);
+        const language = languages.find(
+          item => item.code_iso_3 === languageCode,
+        );
 
         const obj = {
           name,
@@ -152,18 +131,18 @@ export class ImportService {
           telephone,
           timezone,
           domain,
-          continent: continent,
-          currency: currency,
-          language: language,
+          continent: continent?.id,
+          currency: currency?.id,
+          language: language?.id,
           subContinent,
           coords: `${lat},${lng}`,
         };
+        // @ts-ignore
         this.entityManager.save(Country, obj);
-      });
+      }).on('end', () => this.importRegions());
   }
 
   async importRegions() {
-
     const countries = await this.getAllCountries();
 
     await createReadStream('import/regions.csv')
@@ -180,10 +159,11 @@ export class ImportService {
           name,
           nativeName: name,
           code,
-          country: country,
+          country: country?.id,
         };
+        // @ts-ignore
         this.entityManager.save(Region, obj);
-      });
+      }).on('end', () => this.importCities());
   }
 
   async importCities() {
@@ -202,20 +182,16 @@ export class ImportService {
         const obj = {
           name,
           nativeName: name,
-          region: region,
+          region: region?.id,
           coords: `${latitude},${longitude}`,
         };
+        // @ts-ignore
         this.entityManager.save(City, obj);
       });
   }
 
   async importCsv() {
-   const languages = await this.importLanguages();
-    const currencies = await this.importCurrencies();
-    const continents = await this.importContinents();
-    const countries = await this.importCountries();
-    const regions = await this.importRegions();
-    const cities = await this.importCities();
+    await this.startImportCsv();
     return { success: true };
   }
 }
--- src/languages/languages.entity.ts
+++ src/languages/languages.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, OneToMany } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 import { BaseEntity } from '../_base/base.entity';
 import { Country } from '../countries/country.entity';
 import { ApiProperty } from '@nestjs/swagger';
@@ -9,4 +9,8 @@ export class Language extends BaseEntity  {
   @ApiProperty({ type: () => [ Country ] })
   @OneToMany(type => Country, country => country.continent)
   countries: Country[];
+
+  @ApiProperty()
+  @Column({ length: 10, nullable: true })
+  code_iso_3: string;
 }
