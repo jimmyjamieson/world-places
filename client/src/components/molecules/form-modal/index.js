0668e551d879355613bdcbbeee99dc70bcc6f7ab
Ian Jamieson <ian.jamieson@globalgaming.com>
2020-07-18 00:52:34 +0200

    modal toolbar

--- client/src/components/molecules/form-modal/index.js
+++ client/src/components/molecules/form-modal/index.js
@@ -5,12 +5,21 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+}));
 
 const FormModal = ({ open, children, name, onClose }) => {
+  const classes = useStyles();
   return (
     <Dialog open={open} fullWidth>
       <AppBar position="relative">
-        <Toolbar>
+        <Toolbar className={classes.toolbar}>
           <Typography variant="h6">{name}</Typography>
           <IconButton
             edge="start"
@@ -22,7 +31,7 @@ const FormModal = ({ open, children, name, onClose }) => {
           </IconButton>
         </Toolbar>
       </AppBar>
-      { children }
+      {children}
     </Dialog>
   );
 };
