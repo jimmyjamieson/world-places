d18d22c9c1ceb91ac4025aa0fb31c6191274a8ec
Ian Jamieson <ian.jamieson@globalgaming.com>
2020-07-18 21:57:37 +0200

    delete old files

--- client/src/pages/countries/index.js
+++ client/src/pages/countries/index.js
@@ -1,33 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { getCountries } from '../../utils/api';
-import Container from '@material-ui/core/Container';
-import AddFab from '../../components/molecules/add-fab';
-import CountriesForm from '../../components/organisms/countries-form';
-import CountriesList from '../../components/organisms/countries-list';
+import React from 'react';
 
 const CountriesPage = () => {
-  const [continents, setContinents] = useState([]);
-  const [open, setOpen] = useState(false);
-
-  const updateCountries = () => {
-    getCountries().then(res => setContinents(res.data));
-  };
-
-  useEffect(() => {
-    updateCountries();
-  }, []);
-
-  return (
-    <Container>
-      <CountriesForm
-        open={open}
-        close={ () => setOpen(false)}
-        onSuccess={() => updateCountries()}
-      />
-      <CountriesList list={continents} onDeleteSuccess={() => updateCountries()} />
-      <AddFab onClick={() => setOpen(!open)} />
-    </Container>
-  );
+  return 'countries'
 };
 
 export default CountriesPage;
